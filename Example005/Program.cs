a3647055bf73e8c69d2e4031b8e8b14f39e37490
AuthorMax <maxim_terekhin@bk.ru>
2022-02-23 16:07:50 +0300

    Example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yea, it's Masha");
+}
+else 
+{
+    Console.WriteLine("Hello, " + username);
+}
